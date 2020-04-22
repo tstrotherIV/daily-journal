d4949d4866494483c963f511d09f54aef401acb5
Tom Strother <tom_strother@yahoo.com>
2020-04-21 21:30:28 -0500

    New JS File

---
+++ src/scripts/journal.js
@@ -0,0 +1,13 @@
+const journalEntry = {
+  date: "4/21/2020",
+  conceptsCovered: "Manipulating the DOM, Ojbects",
+  journalEntryText:
+    "Today, like most days was difficult but rewarding. Must keep pushing forward!",
+  todaysMood: "Crazy"
+};
+
+let allJournalEntries = [];
+
+allJournalEntries.push(journalEntry);
+
+console.log(allJournalEntries);
